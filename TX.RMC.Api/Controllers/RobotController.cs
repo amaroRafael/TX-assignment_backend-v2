469838b0de5f7654f1ae8603d9c65f639a88bae8
Rafael Amaro <rafael@ratech.jp>
2025-02-10 14:30:52 +0900

    Create Api method to add a new robot.

---
+++ TX.RMC.Api/Controllers/RobotController.cs
@@ -0,0 +1,42 @@
+﻿namespace TX.RMC.Api.Controllers;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using TX.RMC.BusinessLogic;
+using TX.RMC.DataAccess.Core.Models;
+
+[Route("[controller]")]
+[ApiController]
+[Authorize]
+[Produces("application/json")]
+public class RobotController(RobotService robotService, ILogger<RobotController> logger) : ApiBaseController
+{
+    private readonly RobotService robotService = robotService;
+    private readonly ILogger<RobotController> logger = logger;
+    private static readonly string error = "Operation could not be executed at this moment.";
+
+    /// <summary>
+    /// Adds a new robot to the system.
+    /// </summary>
+    /// <param name="name">Robot name identity.</param>
+    /// <returns>Returns the robot details.</returns>
+    [HttpPost]
+    [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(ApiResponse<Robot>), Description = "Returns the robot details.")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, Type = typeof(ErrorResponse), Description = "If there is an error during the process.")]
+    [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "If the user is not authenticated.")]
+    public async Task<IActionResult> Post([FromForm(Name = "name")] string name)
+    {
+        try
+        {
+            Robot robot = await this.robotService.AddAsync(name, HttpContext.RequestAborted);
+            return Ok(robot);
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Error executing [Post] method to add a new robot.");
+            return BadRequest(CreateErrorResponse(error));
+        }
+    }
+}
